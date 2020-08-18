c14463d2e1de316aa64266ac063542d1eca577f1
Anderson Leandro <65420034+AndersonLeandrog@users.noreply.github.com>
2020-08-18 19:30:47 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
-//add a  js code.
+function openMenu() {
+    var menu = document.querySelector('.mobile');
+    menu.style.animation="openAnim 0.2s ease 1";
+    menu.style.animationFillMode="both";
+}
+
+function closeMenu() {
+    var menu = document.querySelector('.mobile');
+    menu.style.animation="closeAnim 0.2s ease 1";
+}
