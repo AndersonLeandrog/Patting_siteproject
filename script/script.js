a21b152442f1f8d8736f9564389040011a458afb
Anderson Leandro <65420034+AndersonLeandrog@users.noreply.github.com>
2020-08-22 15:45:08 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,14 +1,1 @@
-/*a função openMenu seleciona o elemento .mobile e aplica a animação openAnim ao elemento,
-redimensionando ela para uma escala maior em pixels*/
-function openMenu() {
-    var menu = document.querySelector('.mobile');
-    menu.style.animation="openAnim 0.2s ease 1";
-    menu.style.animationFillMode="both";
-}
-
-/*a função closeMenu seleciona o elemento .mobile e aplica a animação closeAnim ao elemento,
-redimensionando ela para uma escala menor em pixels*/
-function closeMenu() {
-    var menu = document.querySelector('.mobile');
-    menu.style.animation="closeAnim 0.2s ease 1";
-}
+//nothing here!
